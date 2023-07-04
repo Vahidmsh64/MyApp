2cde4cca8d88074f49c3ebe931eba0927f4087be
vahidmsh <vahidmsh.outlook.com>
2023-07-04 17:45:42 +0330

    add binding to MainActivity

--- app/src/main/java/com/example/mygittest/MainActivity.kt
+++ app/src/main/java/com/example/mygittest/MainActivity.kt
@@ -2,11 +2,23 @@ package com.example.mygittest
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.mygittest.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private var _binding:ActivityMainBinding? = null
+    private val binding get() = _binding!!
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        _binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.apply {
+
+        }
         // aaa
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
     }
 }
